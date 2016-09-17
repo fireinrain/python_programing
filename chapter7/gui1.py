b8ba274312b450b5ecd19c87cb3a3b3413e21111
lzhaoyang <575563079@qq.com>
2016-09-17 21:51:50 +0800

    update

---
+++ chapter7/gui1.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+# _encoding:utf-8_
+# Written by liuzhaoyang
+
+from tkinter import Label
+
+widget=Label(None,text='hello,刘朝阳')
+widget.pack()
+widget.mainloop()
---
+++ chapter7/gui2.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+# _encoding:utf-8_
+# Written by liuzhaoyang
+
+from tkinter import *
+
+root=Tk()
+Label(root,text='hello,world').pack(side=TOP,expand=YES,fill=BOTH)
+root.mainloop()
