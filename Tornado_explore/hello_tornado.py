fca8e9d38eab2e988620b2a7604b5abcc48a527b
lzhaoyang <575563079@qq.com>
2016-10-21 18:39:11 +0800

    update

---
+++ Tornado_explore/hello_tornado.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+# _encoding:utf-8_
+# Written by liuzhaoyang
+# wcontact:liu575563079@gmail.com
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
