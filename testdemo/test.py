7b9f024baf4d70c77d89045d95404a86ae18ecb7
lzhaoyang <575563079@qq.com>
2016-10-27 23:05:14 +0800

    update

--- testdemo/test.py
+++ testdemo/test.py
@@ -817,19 +817,25 @@
 
 
 # tornado框架尝试
-import tornado.ioloop
-import tornado.web
+# import tornado.ioloop
+# import tornado.web
+#
+# class MainHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.write("Hello, world")
+#
+# def make_app():
+#     return tornado.web.Application([
+#         (r"/", MainHandler),
+#     ])
+#
+# if __name__ == "__main__":
+#     app = make_app()
+#     app.listen(8888)
+#     tornado.ioloop.IOLoop.current().st
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
-
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-
-if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+# import urllib.parse
+#
+# string="你好"
+# ss=urllib.parse.quote(string)
+# print(ss)
