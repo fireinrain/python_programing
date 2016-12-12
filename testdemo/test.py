90cd7544fff990df05d971c9c19b4325358ab69c
lzhaoyang <575563079@qq.com>
2016-12-12 10:00:15 +0800

    add some examples about python_net

--- testdemo/test.py
+++ testdemo/test.py
@@ -1613,17 +1613,31 @@
 # print(lint)
 #
 
-import aiohttp
-import asyncio
+# import aiohttp
+# import asyncio
+#
+# @asyncio.coroutine
+# def print_page(url):
+#     response=yield from aiohttp.request('GET',url)
+#     body=yield from response.read_and_close(decode=True)
+#     print(body)
+#
+# loop=asyncio.get_event_loop()
+# loop.run_until_complete(print_page('http://www.baidu.com'))
 
-@asyncio.coroutine
-def print_page(url):
-    response=yield from aiohttp.request('GET',url)
-    body=yield from response.read_and_close(decode=True)
-    print(body)
+import argparse
 
-loop=asyncio.get_event_loop()
-loop.run_until_complete(print_page('http://www.baidu.com'))
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                   help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                   const=sum, default=max,
+                   help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+
+
 
 
 
